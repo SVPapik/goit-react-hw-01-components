dc4fe6c563da6ca62adad66fd496d9733192d40e
Vitaliy <fireflier87@gmail.com>
2022-10-09 19:43:53 +0300

    shadow

--- src/components/Friends/FriendCard/FriendCard.styled.jsx
+++ src/components/Friends/FriendCard/FriendCard.styled.jsx
@@ -8,12 +8,19 @@ export const Item = styled.li`
   background-color: #fff;
   border-radius: 8px;
   border: 2px solid purple;
-  box-shadow: rgba(196, 39, 198, 0.3) 0px 1px 2px 0px,
-    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  box-shadow: rgba(106, 2, 125, 0.637) 0px -23px 25px 0px inset,
+    rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+    rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px,
+    rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px,
+    rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 400ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
 `;
 
 export const Name = styled.p`
-  margin-left: 8px;
   font-weight: bold;
   /* text-decoration: ${props => (props.isOnline ? 'underline' : 'none')}; */
 `;
